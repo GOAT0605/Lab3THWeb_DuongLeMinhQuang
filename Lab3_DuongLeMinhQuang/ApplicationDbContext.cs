fb715d731969f60f05cbd1ba21d1462fdb63ff36
GOAT0605 <tgdd0605@gmail.com>
2024-03-27 13:16:49 +0700

    Add project files.

---
+++ Lab3_DuongLeMinhQuang/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Lab3_DuongLeMinhQuang.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Product> Products { get; set; }
+	public DbSet<Category> Categories { get; set; }
+	public DbSet<ProductImage> ProductImages { get; set; }
+}
